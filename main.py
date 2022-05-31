74d8d550b56c0c4158857f6a1c32682872d388a0
Federico <38073413+Hexagan@users.noreply.github.com>
2022-05-31 20:08:26 -0300

    Main Actualizado
    Modifiqué el sistema de turnos y ahora tiene un sistema de colas como corresponde. Cola_turnos[0 (PRIMERO)] es el turno actual. Ahora para cambiar de turno, en vez de intercambiar valores con desenpaquetamiento, agrego el contenido de la primera posición (turno actual) al final de la cola, y saco la primera con pop.
    
    El intercambio de turnos al final ahora lo hace solo si el juego cicló al menos una vez. (orden_turnos)
    
    Mostrar a quién le toca primero ahora solo se muestra si es de a 2 jugadores
    
    El condicional extra en la línea 66 es porque cuando no adivinan, los turnos cambiaban y los puntajes se aplicaban al revés.

--- main.py
+++ main.py
@@ -12,34 +12,38 @@ from funciones.resultado import resultado
 
 def main():
     iniciar_partida = True
+    orden_turnos = False
     LIMITE_INTENTOS = 5
+    PRIMERO = 0
+    SEGUNDO = 1
     modo_juego = 0
-    usuario_2 = ""
+    cola_turnos = ['','']
     tabla = {}
 
     # No sale del ciclo hasta seleccionar un modo válido
     while modo_juego not in ['1', '2']:
         modo_juego = input("Introduzca 1 para jugar modo individual o 2 para modo 2 jugadores: ")
+        
+    usuario_1 = input("Usuario 1: Ingrese su nombre para empezar a jugar: ").capitalize()
+    cola_turnos[PRIMERO] = usuario_1
 
-    if modo_juego == '1':
-        usuario_1 = input("Usuario 1: Ingrese su nombre para empezar a jugar: ").capitalize()
-    else:
-        nombre_usuario_1 = input("Usuario 1: Ingrese su nombre para empezar a jugar: ").capitalize()
-        nombre_usuario_2 = input("Usuario 2: Ingrese su nombre: ").capitalize()
+    if modo_juego == '2':
+        usuario_2 = input("Usuario 2: Ingrese su nombre: ").capitalize()
         # Se determina el usuario que inicia la partida
-        usuario_1 = random.choice((nombre_usuario_1, nombre_usuario_2))
-        if usuario_1 == nombre_usuario_1:
-            usuario_2 = nombre_usuario_2
+        if(orden_turnos == False):
+            cola_turnos[PRIMERO] = random.choice((usuario_1, usuario_2))
+            
+        if cola_turnos[PRIMERO] == usuario_1:
+            cola_turnos[SEGUNDO] = usuario_2
         else:
-            usuario_2 = nombre_usuario_1
-
+            cola_turnos[SEGUNDO] = usuario_1
+        print(f"{usuario_1} juega primero!", end="\n\n")
+        
     while iniciar_partida:
         # Condiciones iniciales de cada partida
         PALABRA_A_ADIVINAR = random.choice(obtener_palabras_validas()).upper()
         LONGITUD_PALABRA = len(PALABRA_A_ADIVINAR)
         print(PALABRA_A_ADIVINAR)
-        primer_turno = usuario_1
-        segundo_turno = usuario_2
         intentos = 0
         arriesgo = ""
         coincidencias = []
@@ -48,45 +52,43 @@ def main():
             coincidencias.append("?")
         coincidencias_parciales = []
 
-        print(f"{usuario_1} juega primero!", end="\n\n")
-
         tiempo_inicio = iniciar_cronometro()
         # Validación de arriesgos e intentos durante cada partida
         while intentos <= LIMITE_INTENTOS and arriesgo != PALABRA_A_ADIVINAR:
             actualizar_coincidencias(LIMITE_INTENTOS, coincidencias_parciales, LONGITUD_PALABRA)
             if intentos < LIMITE_INTENTOS:
-                arriesgo = unidecode(input(f"{usuario_1}, tu arriesgo: ").upper(), "utf-8")
+                arriesgo = unidecode(input(f"{cola_turnos[PRIMERO]}, tu arriesgo: ").upper(), "utf-8")
                 arriesgo_valido = validar_arriesgo(arriesgo, LONGITUD_PALABRA, obtener_color)
                 if arriesgo_valido:
                     resultado_parcial, coincidencias = presentar(PALABRA_A_ADIVINAR, arriesgo, obtener_color, coincidencias)
                     coincidencias_parciales.append(resultado_parcial)
                     print(f"{resultado_parcial}\n")
-                    if modo_juego == '2' and arriesgo != PALABRA_A_ADIVINAR:
-                        # Cambio de turno actual usando desenpaquetamiento
-                        usuario_1, usuario_2 = usuario_2, usuario_1
+                    if modo_juego == '2' and arriesgo != PALABRA_A_ADIVINAR and intentos != LIMITE_INTENTOS-1:
+                        # Cambio de turno agregando nombre del jugador actual y removiendolo del primer lugar
+                        cola_turnos.append(cola_turnos[PRIMERO])
+                        cola_turnos.pop(PRIMERO)
                     intentos += 1
                 print(f"Palabra a adivinar: {' '.join(coincidencias)}")
             else:
                 print(f"Palabra a adivinar: {PALABRA_A_ADIVINAR}")
-                usuario_1, usuario_2 = usuario_2, usuario_1
                 intentos += 1
-        ronda = actualizar_puntaje(intentos, tabla, usuario_1, usuario_2)
+        ronda = actualizar_puntaje(intentos, tabla, cola_turnos[0], cola_turnos[1])
         tabla = ronda[0]
         puntos = ronda[1]
         tiempo_final = detener_cronometro(tiempo_inicio)
         resultado(arriesgo, PALABRA_A_ADIVINAR, modo_juego, tiempo_final)
-        print(f"{usuario_1}, obtuviste {puntos} puntos. Tenes acumulados {tabla.get(usuario_1)} puntos en total.")
-        print(f"{usuario_2}, perdiste. Tenes acumulados {tabla.get(usuario_2)} puntos en total." if usuario_2 != '' else '')
+        print(f"{cola_turnos[PRIMERO]}, obtuviste {puntos} puntos. Tenes acumulados {tabla.get(cola_turnos[PRIMERO])} puntos en total.")
+        print(f"{cola_turnos[SEGUNDO]}, perdiste. Tenes acumulados {tabla.get(cola_turnos[SEGUNDO])} puntos en total." if modo_juego == '2' else '')
 
         # Continuar jugando ?
         continuar = unidecode(input("¿Continuar jugando? - S/N: ").upper(), "utf-8")
         if continuar == "S":
             if modo_juego == "2":
-                usuario_1, usuario_2 = segundo_turno, primer_turno
+                cola_turnos.append(cola_turnos[PRIMERO])
+                cola_turnos.pop(PRIMERO)
+                orden_turnos= True
         elif continuar == "N":
             iniciar_partida = False
         else:
             print("Opción incorrecta.")
-
-
 main()
