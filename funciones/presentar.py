75995b912e10040b4ffe56e1744a8420419d1ad5
camilaagustinarivero <camilaagustinarivero2@gmail.com>
2022-06-01 11:17:27 -0300

    se muestra el ganador al dejar de jugar

--- funciones/presentar.py
+++ funciones/presentar.py
@@ -5,6 +5,7 @@ Parámetros:
     PALABRA_A_ADIVINAR: cadena que contiene la palabra a adivinar.
     arriesgo: cadena que contiene un arriesgo válido ingresado por el usuario.
     obtener_color: función que obtiene los colores de utiles.py
+    coincidencias:
 Salidas:
     Devuelve las letras del arriesgo coloreadas con sus colores correspondientes y una lista con el arriesgo ingresado.
 """
@@ -13,27 +14,26 @@ Salidas:
 def presentar(PALABRA_A_ADIVINAR, arriesgo, obtener_color, coincidencias):
     resultado = ""
     datos_coincidencias = {}
-    
-    #Inicializa el diccionario con todas las letras en 0
+
+    # Inicializa el diccionario con todas las letras en 0
     for i in range(len(arriesgo)):
         datos_coincidencias[arriesgo[i]] = 0
-    
-    #Busca primero todas las coincidencias y a su vez las cantidades encontradas
+
+    # Busca primero todas las coincidencias y a su vez las cantidades encontradas
     for pos in range(len(arriesgo)):
         letra = arriesgo[pos]
         if letra == PALABRA_A_ADIVINAR[pos]:
             coincidencias[pos] = letra
             datos_coincidencias[letra] += 1
-    
-    #Tomando en cuenta las coincidencias ya encontradas pinta los colores correspondientes
+
+    # Tomando en cuenta las coincidencias ya encontradas pinta los colores correspondientes
     for posicion in range(len(arriesgo)):
         letra2 = arriesgo[posicion]
-        
         if letra2 == PALABRA_A_ADIVINAR[posicion]:
-            resultado += obtener_color ("Verde") + letra2 + " " +obtener_color ("Defecto")
-        elif(letra2 in PALABRA_A_ADIVINAR and letra2 not in arriesgo[0:posicion] and PALABRA_A_ADIVINAR.count(letra2) > datos_coincidencias[letra2]):
-            resultado += obtener_color ("Amarillo") + letra2 + " " + obtener_color ("Defecto")
+            resultado += obtener_color("Verde") + letra2 + " " + obtener_color("Defecto")
+        elif letra2 in PALABRA_A_ADIVINAR and letra2 not in arriesgo[0:posicion] and PALABRA_A_ADIVINAR.count(letra2) > datos_coincidencias[letra2]:
+            resultado += obtener_color("Amarillo") + letra2 + " " + obtener_color("Defecto")
         else:
-            resultado += obtener_color ("Defecto") + letra2 + " " +obtener_color ("Defecto")
-        
-    return resultado + obtener_color ("Defecto"), coincidencias
+            resultado += obtener_color("Defecto") + letra2 + " " + obtener_color("Defecto")
+
+    return resultado + obtener_color("Defecto"), coincidencias
--- main.py
+++ main.py
@@ -17,33 +17,32 @@ def main():
     PRIMERO = 0
     SEGUNDO = 1
     modo_juego = 0
-    cola_turnos = ['','']
+    cola_turnos = ['', '']
     tabla = {}
 
     # No sale del ciclo hasta seleccionar un modo válido
     while modo_juego not in ['1', '2']:
         modo_juego = input("Introduzca 1 para jugar modo individual o 2 para modo 2 jugadores: ")
-        
+
     usuario_1 = input("Usuario 1: Ingrese su nombre para empezar a jugar: ").capitalize()
     cola_turnos[PRIMERO] = usuario_1
 
     if modo_juego == '2':
         usuario_2 = input("Usuario 2: Ingrese su nombre: ").capitalize()
         # Se determina el usuario que inicia la partida
-        if(orden_turnos == False):
+        if not orden_turnos:
             cola_turnos[PRIMERO] = random.choice((usuario_1, usuario_2))
-            
+
         if cola_turnos[PRIMERO] == usuario_1:
             cola_turnos[SEGUNDO] = usuario_2
         else:
             cola_turnos[SEGUNDO] = usuario_1
-        print(f"{usuario_1} juega primero!", end="\n\n")
-        
+        print(f"{cola_turnos[PRIMERO]} juega primero!", end="\n\n")
+
     while iniciar_partida:
         # Condiciones iniciales de cada partida
         PALABRA_A_ADIVINAR = random.choice(obtener_palabras_validas()).upper()
         LONGITUD_PALABRA = len(PALABRA_A_ADIVINAR)
-        print(PALABRA_A_ADIVINAR)
         intentos = 0
         arriesgo = ""
         coincidencias = []
@@ -60,10 +59,11 @@ def main():
                 arriesgo = unidecode(input(f"{cola_turnos[PRIMERO]}, tu arriesgo: ").upper(), "utf-8")
                 arriesgo_valido = validar_arriesgo(arriesgo, LONGITUD_PALABRA, obtener_color)
                 if arriesgo_valido:
-                    resultado_parcial, coincidencias = presentar(PALABRA_A_ADIVINAR, arriesgo, obtener_color, coincidencias)
+                    resultado_parcial, coincidencias = presentar(PALABRA_A_ADIVINAR, arriesgo, obtener_color,
+                                                                 coincidencias)
                     coincidencias_parciales.append(resultado_parcial)
                     print(f"{resultado_parcial}\n")
-                    if modo_juego == '2' and arriesgo != PALABRA_A_ADIVINAR and intentos != LIMITE_INTENTOS-1:
+                    if modo_juego == '2' and arriesgo != PALABRA_A_ADIVINAR and intentos != LIMITE_INTENTOS - 1:
                         # Cambio de turno agregando nombre del jugador actual y removiendolo del primer lugar
                         cola_turnos.append(cola_turnos[PRIMERO])
                         cola_turnos.pop(PRIMERO)
@@ -79,14 +79,21 @@ def main():
         print(f"{cola_turnos[SEGUNDO]}, obtuviste {puntos} puntos. Tenes acumulados {tabla.get(cola_turnos[SEGUNDO])} puntos en total." if modo_juego == '2' else '')
 
         # Continuar jugando ?
-        continuar = unidecode(input("¿Continuar jugando? - S/N: ").upper(), "utf-8")
+        continuar = unidecode(input("¿Desea jugar otra partida? (S/N): ").upper(), "utf-8")
         if continuar == "S":
             if modo_juego == "2":
                 cola_turnos.append(cola_turnos[PRIMERO])
                 cola_turnos.pop(PRIMERO)
-                orden_turnos= True
         elif continuar == "N":
             iniciar_partida = False
+            if modo_juego == '2':
+                ganador = sorted(tabla.items(), key=lambda p: p)
+                if ganador[0][1] == ganador[1][1]:
+                    print("Empate!")
+                else:
+                    print(f"El ganador es {ganador[0][0]}, con un total de {ganador[0][1]} puntos.")
         else:
             print("Opción incorrecta.")
+
+
 main()
