7a83651ffacf9bd5c9872c034a175f3cfa3a33d1
dgonzalezv97 <daniel.gonzalez5@kyndryl.com>
2022-06-17 17:08:24 -0300

    Agregar validaciones de usuarios

--- funciones/escoger_modo.py
+++ funciones/escoger_modo.py
@@ -12,22 +12,35 @@ Salidas:
 
 import random
 
+def validar_usuarios(usuario, lista_usuarios):
+    lista_usuarios.append(usuario)
+    while usuario == '':
+        usuario = input(f"Ingrese un nombre válido para empezar a jugar: ").capitalize()
+        while (len(lista_usuarios) == 2 and lista_usuarios[0] == lista_usuarios[1]):
+            usuario = input(f"Ingrese un nombre válido para empezar a jugar: ").capitalize()
+            lista_usuarios[1] = usuario
+    return usuario
+
+
 def escoger_modo():
     orden_turnos = False
     PRIMERO = 0
     SEGUNDO = 1
     modo_juego = 0
     cola_turnos = ['', '']
+    lista_usuarios = []
 
     # No sale del ciclo hasta seleccionar un modo válido
     while modo_juego not in ['1', '2']:
         modo_juego = input("Introduzca 1 para jugar modo individual o 2 para modo 2 jugadores: ")
 
         usuario_1 = input("Usuario 1: Ingrese su nombre para empezar a jugar: ").capitalize()
+        usuario_1 = validar_usuarios(usuario_1, lista_usuarios)
         cola_turnos[PRIMERO] = usuario_1
 
         if modo_juego == '2':
             usuario_2 = input("Usuario 2: Ingrese su nombre: ").capitalize()
+            usuario_2 = validar_usuarios(usuario_2, lista_usuarios)
             # Se determina el usuario que inicia la partida
             if not orden_turnos:
                 cola_turnos[PRIMERO] = random.choice((usuario_1, usuario_2))
