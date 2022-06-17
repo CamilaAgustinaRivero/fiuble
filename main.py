9a90ada90fb5d63dcaa7fefedf2f6455ca9f22af
dgonzalezv97 <daniel.gonzalez5@kyndryl.com>
2022-06-17 16:03:06 -0300

    Ordenado de modularizacion

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import random
 from unidecode import unidecode
-from utiles import obtener_palabras_validas
-from utiles import obtener_color
+from utiles import obtener_palabras_validas, obtener_color
 from funciones.cronometro import iniciar_cronometro, detener_cronometro
-from funciones.actualizar_coincidencias import actualizar_coincidencias
-from funciones.validar_arriesgo import validar_arriesgo
-from funciones.presentar import presentar
 from funciones.actualizar_puntaje import actualizar_puntaje
 from funciones.resultado import resultado
 from funciones.escoger_modo import escoger_modo
 from funciones.continuar_jugando import continuar_jugando
+from funciones.actualizar_coincidencias import actualizar_coincidencias
+from funciones.validar_arriesgo import validar_arriesgo
+from funciones.presentar import presentar
 
 
 def main():
@@ -22,26 +21,28 @@ def main():
     while iniciar_partida:
         # Condiciones iniciales de cada partida
         PALABRA_A_ADIVINAR = random.choice(obtener_palabras_validas()).upper()
+        print(PALABRA_A_ADIVINAR)
         LONGITUD_PALABRA = len(PALABRA_A_ADIVINAR)
         intentos = 0
         arriesgo = ""
         coincidencias = []
-        
+
+        tiempo_inicio = iniciar_cronometro()
+
         # La lista coincidencias tiene una longitud dinámica
         for incognita in range(LONGITUD_PALABRA):
             coincidencias.append("?")
         coincidencias_parciales = []
 
-        tiempo_inicio = iniciar_cronometro()
         # Validación de arriesgos e intentos durante cada partida
+        
         while intentos <= LIMITE_INTENTOS and arriesgo != PALABRA_A_ADIVINAR:
             actualizar_coincidencias(LIMITE_INTENTOS, coincidencias_parciales, LONGITUD_PALABRA)
             if intentos < LIMITE_INTENTOS:
                 arriesgo = unidecode(input(f"{cola_turnos[PRIMERO]}, tu arriesgo: ").upper(), "utf-8")
                 arriesgo_valido = validar_arriesgo(arriesgo, LONGITUD_PALABRA, obtener_color)
                 if arriesgo_valido:
-                    resultado_parcial, coincidencias = presentar(PALABRA_A_ADIVINAR, arriesgo, obtener_color,
-                                                                 coincidencias)
+                    resultado_parcial, coincidencias = presentar(PALABRA_A_ADIVINAR, arriesgo, obtener_color, coincidencias)
                     coincidencias_parciales.append(resultado_parcial)
                     print(f"{resultado_parcial}\n")
                     if modo_juego == '2' and arriesgo != PALABRA_A_ADIVINAR and intentos != LIMITE_INTENTOS - 1:
