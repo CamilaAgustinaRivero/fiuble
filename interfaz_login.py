087e025c6c1db82718c06d1b86d70e8b18728af9
dgonzalezv97 <daniel.gonzalez5@kyndryl.com>
2022-07-05 10:56:11 -0300

    Agregar funciones de interfaz sin integracion con el main

--- interfaz_login.py
+++ interfaz_login.py
@@ -1,79 +1,45 @@
 from tkinter import *
-from validar import verificar_ingreso_clave
-from actualizar_datos import actualizar_datos_usuarios
+from validaciones_interfaz import verificar_ingreso_clave
+from form_registro import form_registro
 
-def ventana_login():
+def casilla_usuario_y_clave(numero_usuario, fila, marco):
+    #Casilla usuario
+    mensaje_user = Label(marco, text=f"Usuario {numero_usuario}: ")
+    mensaje_user.grid(row=fila, column=0, sticky= "e", padx=10, pady=10)
+    entry_user = Entry(marco)
+    entry_user.grid(row=fila, column=1, padx=10, pady=10)
+    
+    #Casilla de clave
+    entry_pw = Entry(marco, show="*") 
+    mensaje_pw = Label(marco, text="Clave: ")
+    mensaje_pw.grid(row=fila+1, column=0, sticky= "e", padx=10, pady=10)
+    entry_pw.grid(row=fila+1, column=1, padx=10, pady=10)
+    return entry_user, entry_pw
+
+def ventana_login(modo_de_juego):
     raiz = Tk()
     raiz.title("Login Sortija")
     raiz.resizable(False, False)
-    raiz.iconbitmap('interfaz_grafica\logo_wordle.ico')
+    raiz.iconbitmap('logo_wordle.ico')
     raiz.config(bg="blue")
     
     marco = Frame(raiz)
     marco.config(width=300, height=130, bg="green")
     marco.pack()
-
-    #Casilla de Usuario
-    mensaje_user = Label(marco, text="Usuario: ")
-    mensaje_user.grid(row=0, column=0, sticky= "e", padx=10, pady=10)
-
-    entry_user = Entry(marco)
-    entry_user.grid(row=0, column=1, padx=10, pady=10)
     
-    #Casilla de clave
-    mensaje_pw = Label(marco, text="Clave: ")
-    mensaje_pw.grid(row=1, column=0, sticky= "e", padx=10, pady=10)
+    if modo_de_juego == 2:
+        entry_user1, entry_pw1 = casilla_usuario_y_clave(1, 0, marco)
+        entry_user2, entry_pw2 = casilla_usuario_y_clave(2, 2, marco)
+        botonEnvio=Button(marco, text="Entrar", command=lambda:verificar_ingreso_clave(entry_user1.get(), entry_pw1.get(), modo_de_juego, entry_user2.get(), entry_pw2.get()))
+    else:
+        entry_user1, entry_pw1 = casilla_usuario_y_clave(1, 0, marco)
+        botonEnvio=Button(marco, text="Entrar", command=lambda:verificar_ingreso_clave(entry_user1.get(), entry_pw1.get(), modo_de_juego))
 
-    entry_pw = Entry(marco, show="*")
-    entry_pw.grid(row=1, column=1, padx=10, pady=10)
-    
     #Boton enviar formulario
-    
-    botonEnvio=Button(marco, text="Entrar", command=lambda:verificar_ingreso_clave(entry_user.get(),entry_pw.get()))
-    botonEnvio.grid(row=2, column=1, padx=10, pady=10)
+    botonEnvio.grid(row=5, column=1, padx=10, pady=10)
     
     botonRegistro=Button(marco, text="Registrarse", command=lambda:form_registro())
-    botonRegistro.grid(row=3, column=1, padx=10, pady=10)
-    
+    botonRegistro.grid(row=6, column=1, padx=10, pady=10)
+     
     raiz.mainloop()
     
-def form_registro():
-    raiz = Tk()
-    raiz.title("Registro")
-    raiz.resizable(False, False)
-    raiz.iconbitmap('interfaz_grafica\logo_wordle.ico')
-    raiz.config(bg="blue")
-    
-    marco = Frame(raiz)
-    marco.config(width=300, height=130, bg="green")
-    marco.pack()
-
-    #Casilla de Usuario
-    mensaje_user = Label(marco, text="Usuario: ")
-    mensaje_user.grid(row=0, column=0, sticky= "e", padx=10, pady=10)
-
-    entry_user = Entry(marco)
-    entry_user.grid(row=0, column=1, padx=10, pady=10)
-    
-    #Casilla de clave
-    mensaje_pw = Label(marco, text="Clave: ")
-    mensaje_pw.grid(row=1, column=0, sticky= "e", padx=10, pady=10)
-
-    entry_pw = Entry(marco, show="*")
-    entry_pw.grid(row=1, column=1, padx=10, pady=10)
-    
-    #Casilla de reingreso de clave
-    mensaje_reingreso = Label(marco, text="Reingrese Clave: ")
-    mensaje_reingreso.grid(row=2, column=0, sticky= "e", padx=10, pady=10)
-
-    entry_reingreso = Entry(marco, show="*")
-    entry_reingreso.grid(row=2, column=1, padx=10, pady=10)
-    
-    #Boton enviar formulario
-    
-    botonEnvio=Button(marco, text="Enviar Formulario", command=lambda : actualizar_datos_usuarios(entry_user.get(), entry_pw.get(), entry_reingreso.get()))
-    botonEnvio.grid(row=3, column=1, padx=10, pady=10)
-    
-    raiz.mainloop()
-
-ventana_login()
