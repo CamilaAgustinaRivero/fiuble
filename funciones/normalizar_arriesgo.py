9813e9cd5c8691cf2ce7d14adf20a8e554190329
Federico <38073413+Hexagan@users.noreply.github.com>
2022-07-04 22:50:13 -0300

    Optimización
    Hace una verificación inicial previa a entrar en el ciclo repetitivo

--- funciones/normalizar_arriesgo.py
+++ funciones/normalizar_arriesgo.py
@@ -4,26 +4,28 @@ Función: normalizar_palabra
 Parámetros:
     arriesgo: cadena de caracteres que contiene un arriesgo ingresado por el usuario.
 Salidas:
-    Devuelve la palabra ingresada sin tildes para su procesamiento en el programa.
+    Detecta si la palabra no forma parte de los primeros 128 caracteres de ASCII y devuelve la palabra ingresada sin tildes, si las contiene,
+    para su procesamiento en el programa.
 """
 
-
 def normalizar_palabra(arriesgo):
-    resultado = ''
-    vocales_tildadas = ['Á','É','Í','Ó','Ú']
+    if not arriesgo.isascii():
+        resultado = ''
+        vocales_tildadas = ['Á','É','Í','Ó','Ú']
 
-    for caracter in arriesgo:
-        if caracter in vocales_tildadas:
-            if caracter == 'Á':
-                resultado+='A'
-            elif caracter == 'É':
-                resultado+='E'
-            elif caracter == 'Í':
-                resultado+='I'
-            elif caracter == 'Ó':
-                resultado+='O'
-            else: resultado+='U'
-        else: resultado += caracter
+        for caracter in arriesgo:
+            if caracter in vocales_tildadas:
+                if caracter == 'Á':
+                    resultado+='A'
+                elif caracter == 'É':
+                    resultado+='E'
+                elif caracter == 'Í':
+                    resultado+='I'
+                elif caracter == 'Ó':
+                    resultado+='O'
+                else: resultado+='U'
+            else: resultado += caracter
 
-    arriesgo = resultado
+        arriesgo = resultado
+
     return arriesgo
